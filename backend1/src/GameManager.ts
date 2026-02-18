711d69436820dba9d108d65b836dd823a53cee80
Vivek Yadav <vy6751536@gmail.com>
2026-02-18 07:57:46 +0530

    - Create GameManager class to centralize game connection and user management - Refactor index.ts to use GameManager instead of direct message handling

---
+++ backend1/src/GameManager.ts
@@ -0,0 +1,18 @@
+import { WebSocket } from "ws";
+
+export class GameManager{
+  private games:Game[];
+  constructor(){
+this.games=[];
+  } 
+  addUser(socket:WebSocket){
+
+  }
+  
+   removeUser(socket:WebSocket){
+
+  }
+  private handleMessage(){
+
+  }
+}
--- backend1/src/index.ts
+++ backend1/src/index.ts
@@ -1,10 +1,10 @@
  //ws in nodejs
   import {WebSocketServer} from 'ws';
+  import { GameManager } from './GameManager.js';
   const wss=new WebSocketServer({port:8080});
+
+  const gameManager=new GameManager();
   wss.on('connection',function connection(ws){
-    ws.on('error',console.error);
-    ws.on('message',function message(data){
-        console.log('received:%s',data);
-    });
-    ws.send('something');
+   gameManager.addUser(ws)
+    ws.on('disconnect',()=>gameManager.removeUser(ws));
   });
