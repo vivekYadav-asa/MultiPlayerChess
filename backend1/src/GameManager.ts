1be5dbb5329134e56cc756e147cf5b4bea0d655f
Vivek Yadav <vy6751536@gmail.com>
2026-02-18 14:01:22 +0530

    Create messages.ts with game initialization constants

--- backend1/src/GameManager.ts
+++ backend1/src/GameManager.ts
@@ -1,18 +1,33 @@
 import { WebSocket } from "ws";
+import { INIT_GAME } from "./messages.js";
 
 export class GameManager{
   private games:Game[];
+  private pendingUser:WebSocket;
+  private users:WebSocket[];
   constructor(){
 this.games=[];
   } 
   addUser(socket:WebSocket){
-
+    this.users.push(socket);
+    this.addHandler(socket)
   }
   
    removeUser(socket:WebSocket){
+    this.users=this.users.filter(user=>user!==socket);
+    //stop the game here because the user left
 
   }
-  private handleMessage(){
-
+  private addHandler(socket:WebSocket){
+socket.on("message",(data)=>{
+    const message=JSON.parse(data.toString());
+    if(message.type==INIT_GAME){
+        if(this.pendingUser){
+            //start game
+        } else{
+            this.pendingUser=socket;
+        }
+    }
+})
   }
 }
